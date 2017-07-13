d057f75d285273df500b95cb2bff97d192b7f649
安 卢 <laa6203@163.com>
2017-07-13 20:43:43 +0800

    finish adc.c

--- src/main.c
+++ src/main.c
@@ -7,13 +7,14 @@
 #include "timer.h"
 #include "gpio.h"
 #include "pwm_data.h"
+#include "adc.h"
 
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-
+u16 adcx = 0;
 
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
@@ -27,13 +28,15 @@ int main(void)
 	GPIO_PWM_Init();
 	TIM3_Int_Init(999,167);		//2ms
 
-		SysTick->LOAD = 0x1000;
-		SysTick->CTRL = 0x3;
+	SysTick->LOAD = 0x1000;
+	SysTick->CTRL = 0x3;
 	
+	Adc_Init();         //初始化ADC
 	
 	while(1){
-
-	//	set_pwm(1);	//for debug
+	adcx=Get_Adc_Average(ADC_Channel_5,20);//获取通道5的转换值，20次取平均
+	int ad_reg = (adcx * 99/ 4096)   +1;
+	set_pwm(ad_reg);
 
 
 	}
