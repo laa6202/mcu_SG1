1a5a09441087ad3b7cc3c1e0736864b0c987a8ce
安 卢 <laa6203@163.com>
2017-08-14 20:32:36 +0800

    add rcc_modify.c

---
+++ src/rcc_modify.c
@@ -0,0 +1,39 @@
+
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "rcc_modify.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+//hope HSE is 25MHz,but the actual HSE is 8MHz,now modify the RCC PLLM config
+int rcc_modify(void)
+{ 
+	RCC->CFGR &= 0xffffffd;		//sw1 = 0
+	while((RCC->CFGR & RCC_CFGR_SWS_1) == RCC_CFGR_SWS_1);	//wait SW1=0
+	
+	RCC->CR &= (~RCC_CR_PLLON);		//PLL OFF
+	RCC->PLLCFGR &= 0xffffffc0;		//clear PLLM
+	RCC->PLLCFGR |= 0x8;					//PLLM = 8
+	RCC->CR |= RCC_CR_PLLON;			//PLL ON
+	while((RCC->CR & RCC_CR_PLLRDY) != RCC_CR_PLLRDY);	//wait PLL rdy
+	RCC->CFGR |= 1<<1;			//sw1 = 1
+	while((RCC->CFGR & RCC_CFGR_SWS_1) != RCC_CFGR_SWS_1) ;	//wait SW1 = 1;
+	
+	
+	
+	return 0;
+}
+
+
+
+ 
+
+
+
